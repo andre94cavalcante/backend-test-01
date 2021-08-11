3163f6454c78b442897e9432a1a40e6c5898ba87
andre94cavalcante <andre.94.cavalcante@gmail.com>
2021-08-11 16:15:44 -0300

    Getting all the data from csv files and savig it in MongoDB locally. UML Model almost done

---
+++ teste-1/api/DB/mongodb.js
@@ -0,0 +1,165 @@
+const mongoose = require("mongoose");
+const { userSchema } = require("./userSchema");
+const { roleSchema } = require("./roleSchema");
+const { clientSchema } = require("./clientSchema");
+const { moduleClientSchema } = require("./moduleClientSchema");
+const { moduleRoleSchema } = require("./moduleRoleSchema");
+const { modulesSchema } = require("./modulesSchema");
+const { moduleGroupSchema } = require("./moduleGroupSchema");
+
+mongoose.set("useFindAndModify", false);
+
+mongoose.connect("mongodb://localhost:27017/delfosim", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const User = mongoose.model("users", userSchema);
+const Client = mongoose.model("clients", clientSchema);
+const Role = mongoose.model("roles", roleSchema);
+const ModuleClient = mongoose.model("moduleClient", moduleClientSchema);
+const ModuleRole = mongoose.model("moduleRole", moduleRoleSchema);
+const Module = mongoose.model("modules", modulesSchema);
+const ModuleGroup = mongoose.model("moduleGroup", moduleGroupSchema);
+
+const addUsers = async (data) => {
+  const user = await new User(data);
+  user.id = data.id;
+  user.client_id = data.client_id;
+  user.user_name = data.user_name;
+  user.role_id = data.role_id;
+  user.user_email = data.user_email;
+  user.user_phone = data.user_phone;
+  user.user_type = data.user_type;
+  user.creator_user_id = data.creator_user_id;
+  user.creation_time = data.creation_time;
+  user.is_confirmed = data.is_confirmed;
+  user.is_active = data.is_active;
+  user
+    .save()
+    .then((user) => {
+      console.log("Created object in the DB:");
+      console.log(user);
+    })
+    .catch((error) => {
+      console.log("Error!", error);
+    });
+  // return user;
+};
+
+const addRoles = async (data) => {
+  const role = await new Role(data);
+  role.id = data.id;
+  role.client_id = data.client_id;
+  role.role_name = data.role_name;
+  role
+    .save()
+    .then((role) => {
+      console.log("Created object in the DB:");
+      console.log(role);
+    })
+    .catch((error) => {
+      console.log("Error!", error);
+    });
+  // return role;
+};
+
+const addClients = async (data) => {
+  const client = await new Client(data);
+  client.client_id = data.client_id;
+  client.client_name = data.client_name;
+  client.client_tag = data.client_tag;
+  client
+    .save()
+    .then((client) => {
+      console.log("Created object in the DB:");
+      console.log(client);
+    })
+    .catch((error) => {
+      console.log("Error!", error);
+    });
+  // return client;
+};
+
+const addModuleClient = async (data) => {
+  const moduleClient = await new ModuleClient(data);
+  moduleClient.id = data.id;
+  moduleClient.client_id = data.client_id;
+  moduleClient.module_id = data.module_id;
+  moduleClient
+    .save()
+    .then((moduleClient) => {
+      console.log("Created object in the DB:");
+      console.log(moduleClient);
+    })
+    .catch((error) => {
+      console.log("Error!", error);
+    });
+  // return moduleClient;
+};
+
+const addModuleRole = async (data) => {
+  const moduleRole = await new ModuleRole(data);
+  moduleRole.id = data.id;
+  moduleRole.role_id = data.role_id;
+  moduleRole.module_id = data.module_id;
+  moduleRole
+    .save()
+    .then((moduleRole) => {
+      console.log("Created object in the DB:");
+      console.log(moduleRole);
+    })
+    .catch((error) => {
+      console.log("Error!", error);
+    });
+  // return moduleRole;
+};
+
+const addModules = async (data) => {
+  const module = await new Module(data);
+  module.id = data.id;
+  module.name = data.name;
+  module.tag = data.tag;
+  module.route = data.route;
+  module.group_id = data.group_id;
+  module
+    .save()
+    .then((module) => {
+      console.log("Created object in the DB:");
+      console.log(module);
+    })
+    .catch((error) => {
+      console.log("Error!", error);
+    });
+  // return module;
+};
+
+const addModuleGroup = async (data) => {
+  const moduleGroup = await new ModuleGroup(data);
+  moduleGroup.id = data.id;
+  moduleGroup.name = data.name;
+  moduleGroup.tag = data.tag;
+  moduleGroup.route = data.route;
+  moduleGroup.hierarchy_level = data.hierarchy_level;
+  moduleGroup
+    .save()
+    .then((moduleGroup) => {
+      console.log("Created object in the DB:");
+      console.log(moduleGroup);
+    })
+    .catch((error) => {
+      console.log("Error!", error);
+    });
+  // return moduleGroup;
+};
+
+module.exports = {
+  addUsers: addUsers,
+  addRoles: addRoles,
+  addClients: addClients,
+  addModuleClient: addModuleClient,
+  addModuleRole: addModuleRole,
+  addModules: addModules,
+  addModuleGroup: addModuleGroup,
+};
--- teste-1/api/api.js
+++ teste-1/api/api.js
@@ -1,5 +1,15 @@
+const readCSV = require("./readCSV");
+
 module.exports = (app) => {
   app.get("/", (req, res) => {
-    res.json("testantdoi");
+    res.json("API Started");
+  });
+
+  app.get("/users", (req, res) => {
+    readCSV.readAllData().then((data) => {
+      setTimeout(() => {
+        res.send(data);
+      }, 3000);
+    });
   });
 };
